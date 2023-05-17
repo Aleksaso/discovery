5dcfcd509469628c775577dee69aa5c632602eb6
aleks <space42m@gmail.com>
2023-05-17 20:04:32 +0200

    menu burger

--- cellule0.0/ex04/argv.sh
+++ cellule0.0/ex04/argv.sh
@@ -1,2 +1,15 @@
-./argv.sh arg1 arg2 arg3
-
+#! /bin/bash
+if [ $# -eq 0 ];
+then
+	echo "No hay argumentos"
+else
+	cont=0;
+	for i in $@
+	do
+		if [ $cont -lt 3 ];
+		then
+			echo "$i";
+		fi
+		((cont++));
+	done
+fi
